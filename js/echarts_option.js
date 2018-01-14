14aa53ef5a0fad3796d995b19f6622b982d4e1c5
yihe <742681287@qq.com>
2018-01-14 15:38:23 +0800

    over

---
+++ js/echarts_option.js
@@ -0,0 +1,239 @@
+var polyline_option = {
+    title: {
+        text: '市场行情',
+        left: "center",
+        top: "10%"
+    },
+    tooltip: {
+        trigger: 'axis'
+    },
+    legend: {
+        data:["昨日","今日"]
+    },
+    grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+    },
+    toolbox: {
+        feature: {
+            saveAsImage: {},
+        },
+        right:"3%",
+    },
+    xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: ['12/21/17','12/22/17','12/23/17','12/25/17','12/26/17','12/27/17','12/28/17']
+    },
+    yAxis: {
+        type: 'value',
+        axisLabel:{formatter:'{value} USD'},
+        data: [0, 0.1, 0.2, 02, 0.3, 0.4, 0.5],
+    },
+    series: [
+        {
+            name:'昨日',
+            type:'line',
+            stack: '总量',
+            data:[0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.4]
+        },
+        {
+            name:'今日',
+            type:'line',
+            stack: '总量',
+            data:[0.3,0.3,0.3,0.3,0.5,0.5,0.5,0.4,0.4]
+        }
+    ]
+};
+var polyline_option_day = {
+    title: {
+        text: '市场行情',
+        left: "center",
+        top: "10%"
+    },
+    tooltip: {
+        trigger: 'axis'
+    },
+    legend: {
+        data:["昨日","今日"]
+    },
+    grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+    },
+    toolbox: {
+        feature: {
+            saveAsImage: {},
+        },
+        right:"3%",
+    },
+    xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: ['12/21/17','12/22/17','12/23/17','12/25/17','12/26/17','12/27/17','12/28/17']
+    },
+    yAxis: {
+        type: 'value',
+        axisLabel:{formatter:'{value} USD'},
+        data: [0, 0.1, 0.2, 02, 0.3, 0.4, 0.5],
+    },
+    series: [
+        {
+            name:'昨日',
+            type:'line',
+            stack: '总量',
+            data:[0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.3,0.4]
+        },
+        {
+            name:'今日',
+            type:'line',
+            stack: '总量',
+            data:[0.3,0.3,0.3,0.3,0.5,0.5,0.5,0.4,0.4]
+        }
+    ]
+};
+var polyline_option_week = {
+    title: {
+        text: '市场行情',
+        left: "center",
+        top: "10%"
+    },
+    tooltip: {
+        trigger: 'axis'
+    },
+    legend: {
+        data:["上周","本周"]
+    },
+    grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+    },
+    toolbox: {
+        feature: {
+            saveAsImage: {},
+        },
+        right:"3%",
+    },
+    xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: ['11/6/17','11/13/17','11/20/17','11/27/17','12/4/17','12/11/17','12/18/17']
+    },
+    yAxis: {
+        type: 'value',
+        axisLabel:{formatter:'{value} USD'},
+        data: [0, 0.1, 0.2, 02, 0.3, 0.4, 0.5],
+    },
+    series: [
+        {
+            name:'上周',
+            type:'line',
+            stack: '总量',
+            data:[0.3,0.1,0.2,0.3,0.3,0.2,0.3,0.3,0.4]
+        },
+        {
+            name:'本周',
+            type:'line',
+            stack: '总量',
+            data:[0.3,0.2,0.3,0.1,0.5,0.2,0.5,0.4,0.4]
+        }
+    ]
+};
+var polyline_option_month = {
+    title: {
+        text: '市场行情',
+        left: "center",
+        top: "10%"
+    },
+    tooltip: {
+        trigger: 'axis'
+    },
+    legend: {
+        data:["上月","本月"]
+    },
+    grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+    },
+    toolbox: {
+        feature: {
+            saveAsImage: {},
+        },
+        right:"3%",
+    },
+    xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: ['1/17','2/17','3/17','4/17','5/17','6/17','7/17']
+    },
+    yAxis: {
+        type: 'value',
+        axisLabel:{formatter:'{value} USD'},
+        data: [0, 0.1, 0.2, 02, 0.3, 0.4, 0.5],
+    },
+    series: [
+        {
+            name:'上月',
+            type:'line',
+            stack: '总量',
+            data:[0.3,0.1,0.1,0.4,0.3,0.2,0.3,0.3,0.4]
+        },
+        {
+            name:'本月',
+            type:'line',
+            stack: '总量',
+            data:[0.2,0.2,0.3,0.1,0.3,0.2,0.5,0.4,0.4]
+        }
+    ]
+};
+var pillar_option = {
+    title: {
+        text: '成交总额（万）',
+        left: "center",
+        top: "10%"
+    },
+    color: ['#19d1c4'],
+    tooltip : {
+        trigger: 'axis',
+        axisPointer : {            // 坐标轴指示器，坐标轴触发有效
+            type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+        }
+    },
+    grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+    },
+    xAxis : [
+        {
+            type : 'category',
+            data: ['12/21/17','12/22/17','12/23/17','12/25/17','12/26/17','12/27/17','12/28/17'],
+            axisTick: {
+                alignWithLabel: true
+            }
+        }
+    ],
+    yAxis : [
+        {
+            type : 'value',
+            axisLabel:{formatter:'{value} 万'},
+        }
+    ],
+    series : [
+        {
+            name:'金额',
+            type:'bar',
+            barWidth: '30%',
+            data:[10, 52, 200, 334, 390, 330, 220]
+        }
+    ]
+};
---
+++ js/tree.js
@@ -0,0 +1,202 @@
+﻿$(function () {
+
+    var json =
+        [{
+            "name": "中科慈航",
+            "code":"ZKCH",
+            "icon": "icon-th",
+            "child": [
+                {
+                    "name": "广州中科慈航",
+                    "icon": "icon-minus-sign",
+                    "code":"GZZKCH",
+                    "parentCode": "ZKCH",
+                    "child": [
+                        {
+                            "name": "广州中科慈航天河区分行",
+                            "code":"GZZKCHTQFH",
+                            "icon": "",
+                            "parentCode": "GZZKCH",
+                            "child": []
+                        }
+                    ]
+                },
+                {
+                    "name": "北京中科慈航",
+                    "icon": "",
+                    "code":"BJZKCH",
+                    "parentCode": "ZKCH",
+                    "child": [
+                        {
+                            "name": "北京中科慈航中城区分行",
+                            "code":"BJZKCHZCFH",
+                            "icon": "",
+                            "parentCode": "BJZKCH",
+                            "child": []
+                        }
+                    ]
+                }
+            ]
+        }, {
+            "name": "中科科技",
+            "code":"ZKKJ",
+            "icon": "icon-th",
+            "child": [
+                {
+                    "name": "广州中科科技",
+                    "code":"GZZKKJ",
+                    "icon": "icon-minus-sign",
+                    "parentCode": "ZKKJ",
+                    "child": [
+                        {
+                            "name": "广州天河中科科技",
+                            "code":"GZTHZKKJ",
+                            "icon": "",
+                            "parentCode": "GZZKKJ",
+                            "child": []
+                        }
+                    ]
+                }
+            ]
+        }];
+
+        var tree_ = [{
+            "name":"118256.01",
+            "date": "2017-01-09",
+            "level": 1,
+            "price": 118256.01,
+            "icon": "icon-chevron-sign-down",
+            "child": [
+                {
+                    "name":"118256.02",
+                    "date": "2017-01-09",
+                    "level": 1,
+                    "parentCode": "118256.01",
+                    "icon": "icon-chevron-sign-down",
+                    "price": 118256.02,
+                    "child": [
+                        {
+                            "name":"118256.04",
+                            "date": "2017-01-09",
+                            "level": 1,
+                            "parentCode": "118256.02",
+                            "icon": "icon-minus-sign",
+                            "price": 118256.04,
+                            "child":[],
+                        },
+                        {
+                            "name":"118256.04",
+                            "date": "2017-01-09",
+                            "level": 1,
+                            "parentCode": "118256.02",
+                            "icon": "icon-minus-sign",
+                            "price": 118256.04,
+                            "child":[],
+                        }
+                    ]
+                },
+                {
+                    "name":"118256.03",
+                    "date": "2017-01-09",
+                    "level": 1,
+                    "parentCode": "118256.01",
+                    "price": 118256.02,
+                    "icon": "icon-chevron-sign-down",
+                    "child":[
+                        {
+                            "name":"118256.05",
+                            "date": "2017-01-09",
+                            "level": 1,
+                            "parentCode": "118256.03",
+                            "price": 118256.05,
+                            "child":[],
+                        }
+                    ],
+                }
+            ]
+        }];
+
+    function tree(data) {
+        for (var i = 0; i < data.length; i++) {
+            var data2 = data[i];
+            if (data[i].icon == "icon-chevron-sign-down") {
+                // $("#rootUL").append("<li data-name='" + data[i].name + "'><span><i class='" + data[i].icon + "'></i> " + data[i].price + "</span></li>");
+                $("#rootUL").append(`<li data-name=${data[i].name}>
+                <span class="bg_color">
+                <i class=${data[i].icon}></i>
+                ${data[i].price}
+                </span>
+                <span>
+                <span class="v_img"></span>
+                <span class="level">${data[i].level}</span>
+                </span>
+                <span>
+                    ${data[i].date}
+                </span>
+                </li>`);
+            } else {
+                var children = $("li[data-name='" + data[i].parentCode + "']").children("ul");
+                if (children.length == 0) {
+                    $("li[data-name='" + data[i].parentCode + "']").append("<ul></ul>")
+                }
+                // $("li[data-name='" + data[i].parentCode + "'] > ul").append(
+                //     "<li data-name='" + data[i].name + "'>" +
+                //     "<span>" +
+                //     "<i class='" + data[i].icon + "'></i> " +
+                //     data[i].price +
+                //     "</span>" +
+                //     "</li>")
+                $("li[data-name='" + data[i].parentCode + "'] > ul").append(
+                `<li data-name=${data[i].name}>
+                <span class="bg_color">
+                <i class=${data[i].icon}></i>
+                ${data[i].price}
+                </span>
+                <span>
+                <span class="v_img"></span>
+                <span class="level">${data[i].level}</span>
+                </span>
+                <span>
+                    ${data[i].date}
+                </span>
+                </li>`)
+            }
+            for (var j = 0; j < data[i].child.length; j++) {
+                var child = data[i].child[j];
+                var children = $("li[data-name='" + child.parentCode + "']").children("ul");
+                if (children.length == 0) {
+                    $("li[data-name='" + child.parentCode + "']").append("<ul></ul>")
+                }
+                // $("li[data-name='" + child.parentCode + "'] > ul").append(
+                //     "<li data-name='" + child.name + "'>" +
+                //     "<span>" +
+                //     "<i class='" + child.icon + "'></i> " +
+                //     child.price +
+                //     "</span>" +
+                //     "</li>")
+                $("li[data-name='" + child.parentCode + "'] > ul").append(
+                `<li data-name=${child.name}>
+                <span class="bg_color">
+                <i class=${child.icon}></i>
+                ${child.price}
+                </span>
+                <span>
+                <span class="v_img"></span>
+                <span class="level">${child.level}</span>
+                </span>
+                <span>
+                    ${child.date}
+                </span>
+                </li>`)
+                var child2 = data[i].child[j].child;
+                tree(child2)
+            }
+            tree(data[i]);
+        }
+
+    }
+
+    tree(tree_)
+
+
+});
